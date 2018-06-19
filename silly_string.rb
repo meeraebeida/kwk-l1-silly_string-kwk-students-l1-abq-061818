48e4defbd8ab57f688957b9c70440398701e7b1d
meeraebeida <meeraebeida3@gmail.com>
2018-06-19 17:12:28 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,7 +1,7 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = "bubble"
+your_favorite_word = "bubble".uppercase
 
 puts "My favorite word is #{your_favorite_word}"
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
